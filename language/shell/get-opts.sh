01e03f1c824430340881139c2570dbf3384fb705
weicheng <weicheng.andy@gmail.com>
2019-12-20 13:56:24 +0800

    update note

--- language/shell/get-opts.sh
+++ language/shell/get-opts.sh
@@ -5,16 +5,17 @@
 # OPTIND: 从1开始， 跟$1有不同，./xxx.sh -f a 当解析到a的时候为3
 # OPTION: 自定义变量
 # OPTARG: 带参数的值
+# 第一个冒号标示屏蔽getopts的错误信息，每个参数后面的冒号标示带参数
 # 选项参数的格式必须是-d val，而不能是中间没有空格的-dval。不支持长选项， 也就是--debug之类的选项
-# 前面带冒号隐藏错误信息，后面带冒号表示带参数
+# 所有选项参数必须写在其它参数的前面，因为getopts是从命令行前面开始处理，遇到非-开头的参数，或者选项参数结束标记--就中止了
 # shift $((OPTIND-1))
-# 所有不认识的变量都放 ? 里面，*就走不到
+# 所有不认识的变量都放?里面. *,? 都是通配符
 while getopts :f:h OPTION;do
     case $OPTION in
         f)  echo $OPTIND opt:$OPTION value:$OPTARG;;
         h)  echo $OPTIND opt:$OPTION value:$OPTARG;;
         :)  echo "Missing argument for $OPTARG";;
-        ?)  echo $OPTIND opt:$OPTION value:$OPTARG;;
+        \?)  echo $OPTIND opt:$OPTION value:$OPTARG;;
         # *)  echo $OPTIND opt:$OPTION value:$OPTARG;;
     esac    
 done
