1d62e0a4960803cbb32079baf40fd88fa422c14e
weicheng <weicheng.andy@gmail.com>
2020-02-22 19:25:06 +0800

    add message typ

--- framework/testing/appium/main.py
+++ framework/testing/appium/main.py
@@ -6,5 +6,4 @@ from wechat.driver2 import WechatDriver2
 if __name__ == "__main__":
     driver = WechatDriver2()
     driver.start()
-    input("按任意键推出...")
     print("正在推出...")
--- framework/testing/appium/wechat/driver2.py
+++ framework/testing/appium/wechat/driver2.py
@@ -14,7 +14,7 @@ class WechatDriver2(object):
     desired_caps = {
         'platformName': 'Android',
         'deviceName': 'HUAWEI',
-        'platformVersion': '10',
+        # 'platformVersion': '10',
         'appPackage': 'com.tencent.mm',
         'appActivity': 'com.tencent.mm.ui.LauncherUI',
         'newCommandTimeout': 120,
@@ -24,14 +24,10 @@ class WechatDriver2(object):
     }
 
     def __init__(self):
-        # Espresso,UIAutomator2,UIAutomator
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
         self.conversions = {}
         self.wait = WebDriverWait(self.driver, 300)
         self.wait_list_page()
-
-    def raw_driver(self):
-        return self.driver
 
     def wait_list_page(self):
         print(self.driver.current_activity)
@@ -42,7 +38,6 @@ class WechatDriver2(object):
         # RelativeLayout(com.tencent.mm:id/bw)->LinearLayout->RelativeLayout(点击区域)->LinearLayout->RelativeLayout,TextView(com.tencent.mm:id/dkb)
         # bottom = self.driver.find_element_by_id('com.tencent.mm:id/bw')
         # tabs = bottom.find_elements_by_xpath('//*[@resource-id="com.tencent.mm:id/dkb"]/../..')
-        # bottom.screenshot("bottom.png")
         tabs = self.driver.find_elements_by_xpath('//*[@resource-id="com.tencent.mm:id/dkb"]/../..')
         selected_tab = None
         if len(tabs) == 4:
@@ -64,54 +59,59 @@ class WechatDriver2(object):
             ActionChains(self.driver).move_to_element(selected_tab).click().pause(0.25).click().perform()
         else:
             ActionChains(self.driver).move_to_element(selected_tab).click().perform()
-        time.sleep(0.1)
 
     def conversion_list(self):
         self.select_tab("消息", count=2)
-        listview = self.driver.find_element_by_id("com.tencent.mm:id/dcf")
-        rows = listview.find_elements_by_id("com.tencent.mm:id/bah")
-        has_new_message = False
-        for row in rows:
-            r = self.conversion_detail(row)
-            has_new_message = has_new_message or r
-        if has_new_message:
-            post_runnable(self.conversion_list)
-
-    def conversion_detail(self, row):
-        message_count = ""
-        title = ""
-        last_message = ""
-        # 单聊消息数
-        els = row.find_elements_by_id("com.tencent.mm:id/op")
-        if len(els) > 0:
-            message_count = els[0].get_attribute("text")
-        # 群聊新消息标记
-        els = row.find_elements_by_id("com.tencent.mm:id/bai")
-        if len(els) > 0:
-            message_count = "1+"
-        # 标题
-        els = row.find_elements_by_id("com.tencent.mm:id/baj")
-        if len(els) > 0:
-            title = els[0].get_attribute("text")
-        row.find_elements_by_id("com.tencent.mm:id/bal")
-        if len(els) > 0:
-            last_message = els[0].get_attribute("text")
-        print("Title:{}\nCount:{}\nLast message:{}\n".format(title, message_count, last_message))
-        # row.click()
-        if message_count:
-            TouchAction(self.driver).tap(row).perform()
-            # 进入了详情页面
-            self.conversion_detail_messages(title)
-            # 回调列表页面
-            self.driver.press_keycode(4)
-        return message_count
+        try:
+            listview = self.driver.find_element_by_id("com.tencent.mm:id/dcf")
+            rows = listview.find_elements_by_id("com.tencent.mm:id/bah")
+            has_new_message = False
+            for row in rows:
+                message_count = ""
+                title = ""
+                last_message = ""
+                # 单聊消息数
+                els = row.find_elements_by_id("com.tencent.mm:id/op")
+                if len(els) > 0:
+                    message_count = els[0].get_attribute("text")
+                    has_new_message = True
+                # 群聊新消息标记
+                els = row.find_elements_by_id("com.tencent.mm:id/bai")
+                if len(els) > 0:
+                    message_count = "1+"
+                    has_new_message = True
+                # 标题
+                els = row.find_elements_by_id("com.tencent.mm:id/baj")
+                if len(els) > 0:
+                    title = els[0].get_attribute("text")
+                els = row.find_elements_by_id("com.tencent.mm:id/bal")
+                if len(els) > 0:
+                    last_message = els[0].get_attribute("text")
+                print("Title:{}\nCount:{}\nLast message:{}\n".format(title, message_count, last_message))
+                if message_count:
+                    # row.click()
+                    TouchAction(self.driver).tap(row).perform()
+                    # 进入了详情页面
+                    self.conversion_detail_messages(title)
+                    # 回调列表页面
+                    self.driver.press_keycode(4)
+            if has_new_message:
+                post_runnable(self.conversion_list)
+                return
+        except Exception as e:
+            # 这里是订阅消息等异常情况
+            print(e)
+        input("按任意键推出...")
 
     def conversion_detail_messages(self, room):
-        # TODO
+        # 其它类型的列表有:
+        # 订阅号：com.tencent.mm:id/a9x
+        # 其它：com.tencent.mm:id/ag
         rows = self.driver.find_elements_by_id("com.tencent.mm:id/ab")
+
         a = self.driver.device_time
         print("current time:{}".format(a))
-        current_time = ''
+        current_time = a
         user = ''
         message = ''
         if not self.conversions.get(room):
@@ -125,6 +125,7 @@ class WechatDriver2(object):
                 user = el[0].text
             el = row.find_elements_by_id("com.tencent.mm:id/pq")
             if len(el) > 0:
+                # 文字版本
                 try:
                     ActionChains(self.driver).move_to_element(el[0]).click().pause(0.25).click().perform()
                     full_screen_message_el = self.driver.find_element_by_id("com.tencent.mm:id/awl")
@@ -132,8 +133,37 @@ class WechatDriver2(object):
                     TouchAction(self.driver).tap(self.driver.find_element_by_id("com.tencent.mm:id/awk")).perform()
                 except Exception as e:
                     print(e)
-            print("Time:{}\nUser:{}\nMessage:{}\n".format(current_time, user, message))
-            self.conversions[room].append({"user": user, "time": current_time, "message": message})
+                print("Time:{}\nUser:{}\nMessage:{}\n".format(current_time, user, message))
+                self.conversions[room].append({"user": user, "time": current_time, "message": message})
+                continue
+            el = row.find_elements_by_id("com.tencent.mm:id/aw3")
+            if len(el) > 0:
+                # 图片
+                print("Time:{}\nUser:{}\nMessage:{}\n".format(current_time, user, "一张图片"))
+                continue
+            el = row.find_elements_by_id("com.tencent.mm:id/gi9")
+            if len(el) > 0:
+                # 视频
+                print("Time:{}\nUser:{}\nMessage:{}\n".format(current_time, user, "一个视频"))
+                continue
+            el = row.find_elements_by_id("com.tencent.mm:id/awg")
+            if len(el) > 0:
+                # 语音
+                print("Time:{}\nUser:{}\nMessage:{}\n".format(current_time, user, "一段语音"))
+                continue
+            el = row.find_elements_by_id("com.tencent.mm:id/abv")
+            if len(el) > 0:
+                # 其它
+                print("Time:{}\nUser:{}\nMessage:{}\n".format(current_time, user, "其它信息"))
+                continue
+            # RelativeLayout
+            # --（可能）TextViewcom.tencent.mm:id/ai
+            # --LinearLayout(ab)
+            # ----(分享)LinearLayout(abv)->ata->atb
+            # ----(语音)LinearLayout(atb)->awg
+            # ----(图片)LinearLayout(aw3)->atb
+            # ----(文字)LinearLayout()->pq
+            # ----(视频)LinearLayout()->gi9-abt
         print("==================================")
 
     def conversion_detail_send(self, text):
